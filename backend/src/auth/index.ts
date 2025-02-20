1657d9e17a7768957cd34ced4238d0f66e6eff5e
John4tMoonshot <yonatanat@moonshot.co.il>
2025-02-21 01:23:10 +0200

    add runtime validations

--- backend/src/auth/index.ts
+++ backend/src/auth/index.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import controller from './controller';
+import {
+    validate_login_request,
+    validate_login_response,
+    validate_register_request,
+    validate_register_response,
+} from './middleware';
 
 const auth_router = express.Router();
 
-auth_router.post('/users/login', controller.login);
-auth_router.post('/users', controller.register);
+auth_router.post('/users/login', validate_login_request, controller.login, validate_login_response);
+auth_router.post(
+    '/users',
+    validate_register_request,
+    controller.register,
+    validate_register_response
+);
 auth_router.get('/user/:sessionId', controller.get);
 auth_router.put('/user', controller.update);
 
